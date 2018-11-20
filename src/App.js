fbdbf9c65334b5b2d8fd5bdc2b3dae0bac1a72e2
Xavier <xavier.chaumier@gmail.com>
2018-11-20 16:44:52 +0100

    Struction mise en place

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TextBox from "./components/Textbox";
-import Button from "./components/Button";
+import './components/loginfacebook/Loginfacebook.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nbClicked: 0
-        };
-    }
-
-    handleClick = () => {
-        let newState = this.state.nbClicked;
-        newState++;
-        this.setState({nbClicked: newState});
-    }
-    redoClick = () => {
-        let newState = this.state.nbClicked;
-        newState--;
-        this.setState({nbClicked: newState})
-    }
+class Loginfacebook extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
-            <TextBox nbClicked={(this.state.nbClicked)}/>
-            <Button handleClick={this.handleClick} text={"Plus"}/>
-            <Button handleClick={this.redoClick} text="Moins"/>
-
-        </header>
-      </div>
+    <div className="Test">
+        coucou
+    </div>
     );
   }
 }
 
-export default App;
+export default Loginfacebook;
