fdc72aeaa199dcf8906d51807975881bb1789aab
Xavier <xavier.chaumier@gmail.com>
2018-11-21 14:41:32 +0100

    Facebook connect - Ok Logo - Ok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
 import './components/loginfacebook/Loginfacebook.css';
+import FacebookLogin from 'react-facebook-login';
 
-class Loginfacebook extends Component {
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            picture : "",
+            email : ""
+        }
+    }
 
-  render() {
-    return (
-    <div className="Test">
-        coucoucc
-    </div>
-    );
-  }
+    responseFacebook = (response) => {
+        console.log(response)
+        let picture = response.picture.url;
+        this.setState({picture: response.picture.data.url})
+        let email = response.email;
+        this.setState({email: response.email})
+    }
+    componentClicked = () => {
+    }
+
+
+
+    render() {
+        return (
+                <div className="general">
+                {
+                    this.state.email == "" &&
+                        <img src="https://cdn.discordapp.com/attachments/368420011380834305/514795334480822293/adopte_une_rh_blanc.png"/>
+                }
+                <img src={this.state.picture}/>
+                <p>{this.state.email}</p>
+
+                {
+                    this.state.email == "" &&
+
+                        <div className="">
+                            <FacebookLogin
+                                appId="309196956570599"
+                                autoLoad={true}
+                                fields="name,email,picture"
+                                onClick={this.componentClicked}
+                                callback={this.responseFacebook}/>
+                        </div>
+
+                }
+                </div>
+
+        )
+    }
+
 }
-
-export default Loginfacebook;
+export default App;
